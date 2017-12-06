800a531c655515eec035f20be7bd533e3ea77d6b
ZaoLahma <thyberg.jan@gmail.com>
2017-12-06 19:12:53 +0100

    Added test of rom file contents

--- test/test_emu_rom.c
+++ test/test_emu_rom.c
@@ -1,8 +1,12 @@
 #include "test_emu_rom.h"
 #include "emu_rom.h"
 #include "test_emu_fwk.h"
+#include <string.h>
 
 static char romBuf[50000];
+#define TEST_ROM_CONTENTS "hejhejhejhejhejhejhejhej\n";
+
+static char testRomContents[] = TEST_ROM_CONTENTS;
 
 void EMUROM_test(void)
 {
@@ -10,4 +14,5 @@ void EMUROM_test(void)
 
   TEST_ASSERT_INT_EQ(EMUROM_OK, EMUROM_read("TestRom.nes", romBuf, &romSize));
   TEST_ASSERT_INT_EQ(25u, romSize);
+  TEST_ASSERT_INT_EQ(0u, memcmp(romBuf, testRomContents, romSize));
 }
