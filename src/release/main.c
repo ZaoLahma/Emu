da5a66955e117bbd4ed23d1d76fcef9e162ac2d3
ZaoLahma <thyberg.jan@gmail.com>
2017-12-06 21:09:42 +0100

    Restructured launch code somewhat

--- src/release/main.c
+++ src/release/main.c
@@ -8,14 +8,12 @@ int main(int argc, char** argv)
 {
   EMU_DEBUG_ASSERT_INT_EQ(EXPECTED_NUM_ARGUMENTS, argc);
 
-  if(argc == EXPECTED_NUM_ARGUMENTS)
+  char* romPath = argv[1];
+  EMU_init(romPath);
+  EMU_run();
+
+  if(argc != EXPECTED_NUM_ARGUMENTS)
   {
-    char* romPath = argv[1];
-    EMU_init(romPath);
-    EMU_run();
-  }
-  else
-  {
-    (void) printf("Usage: nes path_to_rom\n");
+    (void) printf("Launch failed. Usage: \"nes path_to_rom\"\n");
   }
 }
