f80256263f40952779129c2ede19bac6d8818f4a
ZaoLahma <thyberg.jan@gmail.com>
2017-12-07 22:19:00 +0100

    CPU structure roughly defined. Very much work in progress.

--- inc/emu_cpu.h
+++ inc/emu_cpu.h
@@ -2,15 +2,21 @@
 #define HEADER_EMU_CPU
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef struct
 {
   uint64_t cycles;
+  uint16_t sp;
   uint16_t pc;
-  uint8_t  A;
-  uint8_t  X;
-  uint8_t  Y;
-  uint8_t  sp;
+  uint8_t  a;
+  uint8_t  b;
+  uint8_t  c;
+  uint8_t  d;
+  uint8_t  e;
+  uint8_t  h;
+  uint8_t  l;
+  bool     stateOk;
 } EMUCPU_Context;
 
 void EMUCPU_init(void);
--- src/emu_cpu.c
+++ src/emu_cpu.c
@@ -2,21 +2,79 @@
 #include "emu_debug.h"
 #include "../test/test_emu_cpu.h"
 #include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-static EMUCPU_Context cpuContext;
+#define NUM_INSTRUCTIONS (0xFF)
+#define STACK_SIZE       (0xFFFFu)
+
+#define UNUSED_ARG(arg) \
+{\
+  arg = arg; \
+} \
+
+typedef void (*EMUCPU_InstructionHandler)(EMUCPU_Context* cpu, char* rom);
+
+typedef struct
+{
+  EMUCPU_InstructionHandler handle;
+} EMUCPU_Instruction;
+
+static void illegalInstruction(EMUCPU_Context* cpu, char* rom);
+static void handleNop(EMUCPU_Context* cpu, char* rom);
+static void handleLdSp(EMUCPU_Context* cpu, char* rom);
+
+static uint8_t stack[STACK_SIZE];
+static EMUCPU_Context cpu;
+static EMUCPU_Instruction instructionTable[NUM_INSTRUCTIONS];
+
+static void illegalInstruction(EMUCPU_Context* cpu, char* rom)
+{
+  uint8_t op = rom[cpu->pc];
+  (void) printf("illegal instruction 0x%x at 0x%x\n", op, cpu->pc);
+  UNUSED_ARG(cpu);
+  UNUSED_ARG(rom);
+  cpu->stateOk = false;
+}
+
+static void handleNop(EMUCPU_Context* cpu, char* rom)
+{
+  UNUSED_ARG(rom);
+  cpu->pc += 1u;
+}
+
+static void handleLdSp(EMUCPU_Context* cpu, char* rom)
+{
+  cpu->sp = rom[cpu->pc + 2u] << 8u | rom[cpu->pc + 1u];
+  cpu->pc += 3u;
+}
 
 void EMUCPU_init()
 {
-  (void) memset(&cpuContext, 0, sizeof(cpuContext));
-  cpuContext.sp = 255u;
+  (void) memset(&cpu, 0, sizeof(cpu));
+  cpu.sp = STACK_SIZE;
+  cpu.pc = 0u;
+  cpu.stateOk = true;
+
+  for(uint32_t i = 0; i < NUM_INSTRUCTIONS; ++i)
+  {
+    instructionTable[i].handle = illegalInstruction;
+  }
+
+  instructionTable[0x0].handle = handleNop;
+  instructionTable[0x31].handle = handleLdSp;
 }
 
 void EMUCPU_run(char* prog)
 {
   EMU_DEBUG_ASSERT_COND(prog);
+  uint8_t op = prog[cpu.pc];
+  (void) printf("op: %u\n", op);
+  instructionTable[op].handle(&cpu, prog);
+  EMU_DEBUG_ASSERT_COND(cpu.stateOk);
 }
 
 void EMUCPU_getContext(struct EMUCPU_Context** context)
 {
-  *context = (struct EMUCPU_Context*)&cpuContext;
+  *context = (struct EMUCPU_Context*)&cpu;
 }
